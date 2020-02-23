301c5109767149d7256247d668edfbe57dc53461
unknown <shabankhawar99@gmail.com>
2020-02-23 06:43:07 +0500

    added some styling with materializecss

--- showitem.js
+++ showitem.js
@@ -5,8 +5,10 @@ const ul = document.querySelector("ul");
 
 //add item
 ipcRenderer.on("item:add", function(e, item) {
+  ul.className = "collection";
   const li = document.createElement("li");
   const itemText = document.createTextNode(item);
+  li.className = "collection-item";
   li.appendChild(itemText);
   ul.appendChild(li);
 });
@@ -14,8 +16,12 @@ ipcRenderer.on("item:add", function(e, item) {
 //clear items
 ipcRenderer.on("item:clear", function() {
   ul.innerHTML = "";
+  ul.className = "";
 });
 
 ul.addEventListener("dblclick", e => {
   e.target.remove();
+  if (ul.innerHTML == "") {
+    ul.className = "";
+  }
 });
